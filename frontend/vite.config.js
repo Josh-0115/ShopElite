dd5800f8e49b0ea6ca5f5b5585daa48f5d31b08d
Josh-0115 <sunnyjosh0115@gmail.com>
2025-08-21 11:48:04 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,17 +8,4 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
-  '/images': {
-       target: 'http://localhost:3001',
-       changeOrigin: true,
-  }
 })
